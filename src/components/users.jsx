9d255590a6e8eed561747ee5cc4a6de4b553d667
GawainTolle <artemsq77@gmail.com>
2022-09-23 20:33:29 +0700

    updatedStatus

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
 import api from "../api";
 
-const Users = (props) => {
+const Users = () => {
      const [users, setUsers] = useState(api.users.fetchAll());
+     console.log(users);
      const handleDelete = (userId) => {
-          const newUsers = users.filter((person) => person.id !== userId);
+          const newUsers = users.filter((person) => person.userId !== userId);
           setUsers(newUsers);
      };
-     const renderPhrase = (number) => {};
+     const getUser = users.map((user) => {
+          return user;
+     });
+     // console.log("getuser");
+     const renderPhrase = (number) => {
+          return (
+               getUser.length && (
+                    <ul>
+                         {users.map((user) => (
+                              <li className="btn btn-primary btn-sm m-2">{user.length}</li>
+                         ))}
+                    </ul>
+               )
+          );
+     };
+     const desingQualities = () => {
+          // let desing = "badge m-2";
+          const retriveDesing = users.map((user) => {
+               return user.qualities.map((c) => {
+                    return c.color;
+               });
+          });
+          return `badge bg-${retriveDesing}`;
+     };
      return (
           <>
                <table class="table">
                     <thead>
+                         {renderPhrase()}
                          <tr>
                               <th scope="col">Имя</th>
                               <th scope="col">Качество</th>
@@ -21,22 +46,27 @@ const Users = (props) => {
                          </tr>
                     </thead>
                     <tbody>
-                         <tr>
-                              <td>{users.name}</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                         </tr>
-                         <tr>
-                              <th scope="row">2</th>
-                              <td>Jacob</td>
-                              <td>Thornton</td>
-                              <td>@fat</td>
-                         </tr>
-                         <tr>
-                              <th scope="row">3</th>
-                              <td colspan="2">Larry the Bird</td>
-                              <td>@twitter</td>
-                         </tr>
+                         {users.map((user) => {
+                              return (
+                                   <tr>
+                                        <td>{user.name}</td>
+                                        <td className={desingQualities}>
+                                             {user.qualities.map((u) => {
+                                                  return u.name;
+                                             })}
+                                        </td>
+                                        <td>{user.profession.name}</td>
+                                        <td>{user.completedMeetings}</td>
+                                        <td>{user.rate}</td>
+                                        <td>
+                                             {" "}
+                                             <button className="btn btn-danger btn-sm m-2" onClick={handleDelete}>
+                                                  delete
+                                             </button>
+                                        </td>
+                                   </tr>
+                              );
+                         })}
                     </tbody>
                </table>
           </>
